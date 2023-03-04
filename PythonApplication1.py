656d8213a2c627e2eb787828f52f4656a9be1ef9
Zeynel Abidin Çiftçioğlu <84420233+MrZeynel@users.noreply.github.com>
2023-03-05 00:03:15 +0300

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,41 @@
+﻿print ("""
+Hesaplama makinesi
+
+Lütfen işleminizi seçiniz:
+
+1 Toplama
+2 Çıkarma
+3 Çarpma
+4 Bölme
+F Çıkış
+""")
+
+while True:
+
+    islem = str(input("İşlem Seçiniz:"))
+
+    if islem == "1":
+     sayi1 = int(input("Birincil Sayı: "))
+     sayi2 = int(input("İkincil Sayı: "))
+     print ("\n\nSonuç: ", sayi1 + sayi2)
+
+    elif islem == "2":
+        sayi1 = int(input("Birincil Sayı: "))
+        sayi2 = int(input(" İkincil Sayı: "))
+        print ("\n\nSonuç: ", sayi1 - sayi2)
+
+    elif islem == "3":
+        sayi1 = int(input("Birincil Sayı: "))
+        sayi2 = int(input(" İkincil Sayı: "))
+        print ("\n\nSonuç: ", sayi1 * sayi2)
+
+    elif islem == "4":
+        sayi1 = int(input("Birincil Sayı: "))
+        sayi2 = int(input(" İkincil Sayı: "))
+        print ("\n\nSonuç: ", sayi1 / sayi2)
+
+    elif islem == "F":
+         break
+
+    else:
+     print("Yanlış seçenek!")
